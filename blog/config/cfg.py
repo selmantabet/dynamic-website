ea8d744ee57430b78044a42e568f68949e1318b5
Selman N. Tabet <33247400+selmantabet@users.noreply.github.com>
2023-01-11 01:00:02 +0000

    Production Config Update

--- blog/config/cfg.py
+++ blog/config/cfg.py
@@ -19,20 +19,8 @@ if (os.path.exists(dev_env_path)):
     load_dotenv(dotenv_path=dev_env_path)
     print("dev env vars initialized from config")
 
-prod_env_path = os.path.join(cwd, "blog", "env", "prod.env")
-print("prod env path: ", prod_env_path)
-if (os.path.exists(prod_env_path)):
-    load_dotenv(dotenv_path=prod_env_path)
-    print("prod env vars initialized from config")
-
-if os.environ["DB_MODE"] == "postgres":
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-        dbuser=os.environ["POSTGRESQL_USER"],
-        dbpass=os.environ["POSTGRESQL_PASSWORD"],
-        dbhost=os.environ["POSTGRESQL_ADDRESS"],
-        dbname=os.environ["POSTGRESQL_DB_NAME"]
-    )
-elif os.environ["DB_MODE"] == "mysql":
+if os.environ.get("ENV_TYPE") == "GITLAB":
+    print("GITLAB deployment detected.")
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
         dbuser=os.environ["MYSQL_USER"],
         dbpass=os.environ["MYSQL_PASSWORD"],
@@ -40,5 +28,26 @@ elif os.environ["DB_MODE"] == "mysql":
         dbname=os.environ["MYSQL_DB_NAME"]
     )
 else:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-        os.path.join(cwd, "blog", 'blog.db')
+    prod_env_path = os.path.join(cwd, "blog", "env", "prod.env")
+    print("prod env path: ", prod_env_path)
+    if (os.path.exists(prod_env_path)):
+        load_dotenv(dotenv_path=prod_env_path)
+        print("prod env vars initialized from config")
+
+    if os.environ.get("DB_MODE") == "postgres":
+        SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+            dbuser=os.environ["POSTGRESQL_USER"],
+            dbpass=os.environ["POSTGRESQL_PASSWORD"],
+            dbhost=os.environ["POSTGRESQL_ADDRESS"],
+            dbname=os.environ["POSTGRESQL_DB_NAME"]
+        )
+    elif os.environ.get("DB_MODE") == "mysql":
+        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+            dbuser=os.environ["MYSQL_USER"],
+            dbpass=os.environ["MYSQL_PASSWORD"],
+            dbhost=os.environ["MYSQL_ADDRESS"],
+            dbname=os.environ["MYSQL_DB_NAME"]
+        )
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+            os.path.join(cwd, "blog", 'blog.db')
