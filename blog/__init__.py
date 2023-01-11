b517f2a389de03c401b2147aa134b8c1bb636ed8
Selman N. Tabet <33247400+selmantabet@users.noreply.github.com>
2023-01-11 21:59:50 +0000

    Functionality update

--- blog/__init__.py
+++ blog/__init__.py
@@ -10,9 +10,11 @@ app.config['SECRET_KEY'] = '64c81e139e3c113ecf8393abeaf83028196ca82e2acf2a3a'
 # DB Connection changed to mysql
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config.from_object(cfg)
-print("DB URI: ", app.config['SQLALCHEMY_DATABASE_URI'])
+
 # print("Tracking: ", app.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
 db = SQLAlchemy(app)
-
+with app.app_context():
+    db.create_all()
+    print("DB created at ", app.config['SQLALCHEMY_DATABASE_URI'])
 login_manager = LoginManager()
 login_manager.init_app(app)
--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, request, redirect, flash
 from blog import app
 from blog.forms import RegistrationForm, LoginForm
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 
 
 @app.route("/")
@@ -9,7 +9,7 @@ from flask_login import login_user, logout_user, current_user
 def home():
     from blog.models import Post
     posts = Post.query.all()
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', posts=posts, current_user=current_user)
 
 
 @app.route("/about")
@@ -49,17 +49,31 @@ def login():
     if form.validate_on_submit():
         from blog.models import User
         user = User.query.filter_by(username=form.username.data).first()
+        remember = True if request.form.get('remember') else False
         if user is not None and user.verify_password(form.password.data):
-            login_user(user)
-            flash('You\'ve successfully logged in,' +
-                  ' ' + current_user.username + '!')
+            login_user(user, remember=remember)
+            flash(f'You have successfully logged in, {current_user.username}!')
             return redirect(url_for('home'))
         flash('Invalid username or password.')
     return render_template('login.html', title='Login', form=form)
 
 
 @app.route("/logout")
+@login_required
 def logout():
+    flash(f'You have successfully logged out, {current_user.username}!')
     logout_user()
-    flash('You\'re now logged out. Thanks for your visit!')
+    # flash('You are now logged out. Thanks for your visit!')
     return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account Settings')
+
+
+@app.route("/create")
+@login_required
+def create():
+    return render_template('create.html', title='Create Post')
