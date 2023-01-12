afe6ec59c5867f5a9a8dfd4e1d07263f6c671b29
Selman N. Tabet <33247400+selmantabet@users.noreply.github.com>
2023-01-12 00:46:08 +0000

    Minor fix.

--- blog/__init__.py
+++ blog/__init__.py
@@ -10,12 +10,12 @@ app.config['SECRET_KEY'] = '64c81e139e3c113ecf8393abeaf83028196ca82e2acf2a3a'
 # DB Connection changed to mysql
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config.from_object(cfg)
-
+print("DB created at ", app.config['SQLALCHEMY_DATABASE_URI'])
 # print("Tracking: ", app.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
-    print("DB created at ", app.config['SQLALCHEMY_DATABASE_URI'])
+# with app.app_context():
+#     db.create_all()
+
 login_manager = LoginManager()
 login_manager.login_view = 'login'  # Go here if accessed a restricted page
 login_manager.init_app(app)
